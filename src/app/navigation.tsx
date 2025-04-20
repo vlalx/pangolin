aa370ce067f203338812904498d986ee78fae4de
Marvin <127591405+Lokowitz@users.noreply.github.com>
2025-04-20 11:17:13 +0200

    Update navigation.tsx

--- src/app/navigation.tsx
+++ src/app/navigation.tsx
@@ -7,79 +7,64 @@ import {
     Waypoints,
     Combine
 } from "lucide-react";
-import { useTranslations } from 'next-intl';
 
-const RootNavItems = () => {
-    const t = useTranslations('layout');
+export const rootNavItems: SidebarNavItem[] = [
+    {
+        title: "Home",
+        href: "/"
+        // icon: <Home className="h-4 w-4" />
+    }
+];
 
-    return [
-        {
-            title: t('home'),
-            href: "/"
-            // icon: <Home className="h-4 w-4" />
-        }
-    ];
-}
+export const orgNavItems: SidebarNavItem[] = [
+    {
+        title: "Sites",
+        href: "/{orgId}/settings/sites"
+        // icon: <Combine className="h-4 w-4" />
+    },
+    {
+        title: "Resources",
+        href: "/{orgId}/settings/resources"
+        // icon: <Waypoints className="h-4 w-4" />
+    },
+    {
+        title: "Access Control",
+        href: "/{orgId}/settings/access",
+        // icon: <Users className="h-4 w-4" />,
+        autoExpand: true,
+        children: [
+            {
+                title: "Users",
+                href: "/{orgId}/settings/access/users",
+                children: [
+                    {
+                        title: "Invitations",
+                        href: "/{orgId}/settings/access/invitations"
+                    }
+                ]
+            },
+            {
+                title: "Roles",
+                href: "/{orgId}/settings/access/roles"
+            }
+        ]
+    },
+    {
+        title: "Shareable Links",
+        href: "/{orgId}/settings/share-links"
+        // icon: <LinkIcon className="h-4 w-4" />
+    },
+    {
+        title: "Settings",
+        href: "/{orgId}/settings/general"
+        // icon: <Settings className="h-4 w-4" />
+    }
+];
 
-const OrgNavItems = () => {
-    const t = useTranslations('layout');
-
-    return [
-        {
-            title: t('sites'),
-            href: "/{orgId}/settings/sites"
-            // icon: <Combine className="h-4 w-4" />
-        },
-        {
-            title: t('resources'),
-            href: "/{orgId}/settings/resources"
-            // icon: <Waypoints className="h-4 w-4" />
-        },
-        {
-            title: t('access'),
-            href: "/{orgId}/settings/access",
-            // icon: <Users className="h-4 w-4" />,
-            autoExpand: true,
-            children: [
-                {
-                    title: t('users'),
-                    href: "/{orgId}/settings/access/users",
-                    children: [
-                        {
-                            title: t('invitations'),
-                            href: "/{orgId}/settings/access/invitations"
-                        }
-                    ]
-                },
-                {
-                    title: t('roles'),
-                    href: "/{orgId}/settings/access/roles"
-                }
-            ]
-        },
-        {
-            title: t('links'),
-            href: "/{orgId}/settings/share-links"
-            // icon: <LinkIcon className="h-4 w-4" />
-        },
-        {
-            title: t('settings'),
-            href: "/{orgId}/settings/general"
-            // icon: <Settings className="h-4 w-4" />
-        }
-    ];
-}
-
-const AdminNavItems = () => {
-    const t = useTranslations('layout');
-
-    return [
-        {
-            title: t('allUsers'),
-            href: "/admin/users"
-            // icon: <Users className="h-4 w-4" />
-        }
-    ];
-}
-
-export { RootNavItems, OrgNavItems, AdminNavItems };
+export const adminNavItems: SidebarNavItem[] = [
+    {
+        title: "All Users",
+        href: "/admin/users"
+        // icon: <Users className="h-4 w-4" />
+    }
+];
