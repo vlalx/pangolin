7993e23d446f8db7d5140bdf739956d741bf712a
Marvin <127591405+Lokowitz@users.noreply.github.com>
2025-04-24 12:59:46 +0200

    Update LocaleSwitcher.tsx

--- src/components/LocaleSwitcher.tsx
+++ src/components/LocaleSwitcher.tsx
@@ -1,4 +1,4 @@
-import {useLocale, useTranslations} from 'next-intl';
+import { useLocale, useTranslations } from 'next-intl';
 import LocaleSwitcherSelect from './LocaleSwitcherSelect';
 
 export default function LocaleSwitcher() {
